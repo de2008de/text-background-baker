ca7afadf843f49784d950d6deeac433f92c1714b
David Chou <mrycchou@gmail.com>
2020-02-28 21:17:00 -0800

    Add font size slider

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from 'antd';
+import { Button, Slider, Row, Col } from 'antd';
 import 'antd/dist/antd.css';
 import '../css/Home.css';
 
@@ -10,24 +10,27 @@ const defaults = {
     }
 };
 
-const styles = {
-    preview: {
-        height: "300px",
-        width: "450px",
-        margin: "auto"
-    }
-}
-
 const Home: React.FC<{}> = props => {
     const [bgColor, setBgColor] = useState(defaults.color.background);
     const [textColor, setTextColor] = useState(defaults.color.text);
     const [text, setText] = useState("");
+    const [fontSize, setFontSize] = useState(36);
+
+    const styles = {
+        preview: {
+            height: "300px",
+            width: "450px",
+            margin: "auto"
+        },
+        preivewText: {
+            fontSize: fontSize
+        }
+    }
 
     const onMakeHandler = () => {
+
         const preview = document.getElementById("preview-area");
 
-
-        
     };
 
     const onChangeText = (e: any) => {
@@ -41,11 +44,17 @@ const Home: React.FC<{}> = props => {
 
     };
 
+    const onChangeFontSize = (value: any) => {
+
+        setFontSize(value);
+     
+    }
+
     const onChangeColor = (name: string) => {
 
         if (name === "background") {
-        
-            return (e: any):void => {
+
+            return (e: any): void => {
                 const preview = document.getElementById("preview-area");
 
                 if (!preview) {
@@ -55,10 +64,10 @@ const Home: React.FC<{}> = props => {
                 setBgColor(e.target.value);
                 preview.style.backgroundColor = e.target.value;
             }
-           
+
         } else if (name === "text") {
 
-            return (e: any):void => {
+            return (e: any): void => {
                 const preview = document.getElementById("preview-area");
 
                 if (!preview) {
@@ -75,26 +84,40 @@ const Home: React.FC<{}> = props => {
     return (
         <div>
             <div id="preview-area" style={styles.preview}>
-                <p id="preview-text">text</p>
+                <p id="preview-text" style={styles.preivewText}>text</p>
             </div>
 
-            <div className="control color-picker">
-                <label>Background Color</label>
-                <div>
-                    <input type="color" value={bgColor} onChange={onChangeColor("background")}></input>
-                </div>
-            </div>
+            <Row style={{width: "40%", margin: "auto"}}>
+                <Col span={12}>
+                    <div className="control color-picker">
+                        <label>Background Color</label>
+                        <div>
+                            <input type="color" value={bgColor} onChange={onChangeColor("background")}></input>
+                        </div>
+                    </div>
 
-            <div className="control color-picker">
-                <label>Text Color:</label>
-                <div>
-                    <input type="color" value={textColor} onChange={onChangeColor("text")}></input>
-                </div>
-            </div>
+                    <div className="control color-picker">
+                        <label>Text Color:</label>
+                        <div>
+                            <input type="color" value={textColor} onChange={onChangeColor("text")}></input>
+                        </div>
+                    </div>
 
-            <div className="control input-text">
-                <textarea placeholder="enter your text here" value={text} onChange={onChangeText}></textarea>
-            </div>
+                    <div className="control input-text">
+                        <textarea placeholder="enter your text here" value={text} onChange={onChangeText}></textarea>
+                    </div>
+                </Col>
+
+                <Col span={12}>
+                    <label>Font Size</label>
+                    <Slider
+                        min={2}
+                        max={128}
+                        value={fontSize}
+                        onChange={onChangeFontSize}
+                    />
+                </Col>
+            </Row>
 
             <div className="control button-wrapper" onClick={onMakeHandler}>
                 <Button type="primary">Make</Button>
